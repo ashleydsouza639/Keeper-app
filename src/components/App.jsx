d59f88ad4db0d934ed1b68134928d106255e56ae
Ashley Dsouza <ashleydsouza639@gmail.com>
2020-02-20 20:52:25 +0530

    implement add functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
+//import notes from "../notes";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    ////props.onAdd(note)) in CreateArea.jsx  <CreateArea onAdd={addNote} />;
+    //console.log(newNote);
+    setNotes(prevNote => {
+      return [...prevNote, newNote];
+    });
+  }
+
+
+
+
   return (
     <div>
       <Header />
+      <CreateArea onAdd={addNote} />
       {/* add note component */}
-      {notes.map(noteItem => (
+      {notes.map((noteItem,index) => (
         <Note 
-        key = {noteItem.key}
-        title = {noteItem.title}
-        content = {noteItem.content}
+          key={index}
+          id={index}
+          title={noteItem.title}
+          content={noteItem.content}
+     
         />
       ))}
       <Footer />
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Note(props) {
+function Note(props) {     //pass props in child component so you can use .attribute in parent App component
   return (
     <div className="note">
-      <h1>{props.title}</h1>
+      <h1>{props.title}</h1>               {/*   <Note title=""></Note> in App.jsx*/}
       <p>{props.content}</p>
     </div>
   );
